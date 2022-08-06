168b13ff6d1007011c0e5b88c97411b50a01df5d
myz1237 <mengyizhuoprivate@outlook.com>
2022-08-06 15:34:14 +0800

    timer change

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const discord = require('discord.js');
 const fs = require("fs");
 const path = require('path')
-require("dotenv").config()
 
 //Intents mean the functionality of your bot
 const client = new discord.Client({ intents: [
